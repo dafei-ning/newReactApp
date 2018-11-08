8be6c3c51d621aa3bf13bec2a16f2eb6519b2180
Dafei Ning <ningdafei@gmail.com>
2018-11-07 21:27:06 -0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Components/Person';
+import React, { Component } from "react";
+//import logo from './logo.svg';
+import "./App.css";
+import Person from "./Components/Person";
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name : 'Jack', age: 28},
+      { name : 'Echo', age: 28},
+      { name : 'Java', age: 28}
+
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I am Dafei Ning</h1>
-        <Person name = "Jack" age ="28"/>
-        <Person name = "Echo" age ="27">My hobbie is eat bianbian.</Person>
-        <Person name = "Java" age ="26"/>
-
+        <button>Swtich Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name="Echo" age="27">
+          My hobbie is eat bianbian.
+        </Person>
+        <Person name="Java" age="26" />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is BaobaoHui!'));
