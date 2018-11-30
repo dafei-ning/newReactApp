9d483a9751ca4ecaf7b9131110b5bf7d3af3e24f
Dafei Ning <ningdafei@gmail.com>
2018-11-30 01:53:14 -0800

    ->list

--- src/Components/App.js
+++ src/Components/App.js
@@ -48,17 +48,12 @@ class App extends Component {
     if (this.state.showPersons === true) {
       persons = (
         <div >
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "hui")}
-            changed={this.nameChangedHandler}>My hobbie is to eat bianbian.</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age} />
+          })}
+          
         </div>
       );
     }
